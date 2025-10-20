1c35f8485479c0b5b51cc3f61f7cab70c8214d43
Karan09542 <karan09542@gmail.com>
2025-10-20 09:01:37 +0530

    Dream Project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+const Dream = ({name,description, Tag})=>{
+  return <Tag>
+    <p className="project-name" data-ns-test>{name}</p>
+    <div className="project-description" data-ns-test>{description}</div>
+  </Tag>
 }
+const App = () => {
+  const [dreams, setDreams] = useState([
+    {
+      name: "Dream 1",
+      description: "Blissfull Dream",
+    },
+    {
+      name: "Dream 2",
+      description: "Blissfull Dream",
+    },
+    {
+      name: "Dream 3",
+      description: "Blissfull Dream",
+    },
+    {
+      name: "Dream 4",
+      description: "Blissfull Dream",
+    },
+    {
+      name: "Dream 5",
+      description: "Blissfull Dream",
+    },
+    {
+      name: "Dream 6",
+      description: "Blissfull Dream",
+    },
+  ]);
 
+  return <div id="main ns-wrapper">
+    {dreams.map((dream,i)=> <Dream {...dream} Tag={`h${i+1}`}/>)}
+  </div>;
+};
 
 export default App;
